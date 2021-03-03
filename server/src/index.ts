45930878c4d81e677fb09d21b670c6134d235984
Ignacio Morales <moralesignaciojoaquin@gmail.com>
2021-03-03 12:23:52 -0300

    server: Initial setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,9 +4,9 @@ import "dotenv-safe/config";
 const app = express();
 app.get('/', (req, res) => {
   console.log(req.body);
-  res.send('Hello world!!!');
+  res.send('Now nodemon is working!');
 })
 
-app.listen(parseInt(process.env.PORT), () => {
+app.listen(parseInt(process.env.PORT) || 8080, () => {
   console.log(`Server started on localhost:${process.env.PORT}`);
 });
